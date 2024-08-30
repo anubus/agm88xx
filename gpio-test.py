11b5de294d05ea34e1c1464fdf6e21f425f15f1f
Robert Bolling <anubus32@gmail.com>
2024-08-30 12:45:46 -0400

    button test

--- gpio-test.py
+++ gpio-test.py
@@ -6,8 +6,44 @@ import board
 
 led = digitalio.DigitalInOut(board.D21)
 led.direction = digitalio.Direction.OUTPUT
-while True:
+buttonLL = digitalio.DigitalInOut(board.D26)
+buttonUL = digitalio.DigitalInOut(board.D19)
+buttonLR = digitalio.DigitalInOut(board.D13)
+buttonUR = digitalio.DigitalInOut(board.D6)
+buttonLL.direction = digitalio.Direction.INPUT
+buttonUL.direction = digitalio.Direction.INPUT
+buttonLR.direction = digitalio.Direction.INPUT
+buttonUR.direction = digitalio.Direction.INPUT
+buttonLL.pull = digitalio.Pull.UP
+buttonUL.pull = digitalio.Pull.UP
+buttonLR.pull = digitalio.Pull.UP
+buttonUR.pull = digitalio.Pull.UP
+
+def blink():
     led.value = True
-    time.sleep(0.1)
+    time.sleep(0.2)
     led.value = False
-    time.sleep(0.1)
+    return
+
+print("Button test")
+
+while True:
+    if buttonLL.value == False:
+        print("Lower Left button pressed!")
+        blink()
+ 
+    if buttonUL.value == False:
+        print("Upper Left button pressed!")
+        blink()
+ 
+    if buttonLR.value == False:
+        print("Lower Right button pressed!")
+        blink()
+ 
+    if buttonUR.value == False:
+        print("Uper Right button pressed!")
+        blink()
+ 
+ 
+
+    
